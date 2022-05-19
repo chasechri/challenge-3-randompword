09dbd4b196e5b77b82363c6cb7ff8bd5f58f3927
chasechri <christensonchase451@gmail.com>
2022-05-19 11:39:21 -0500

    functions

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+    console.log("clicked button")
+
+    // 1 prompt user
+    var passwordLength = window.prompt('How many characters would you like your password to be?');
+
+    var confirmSpecialcharacters = window.confirm('Click OK to confirm including special characters');
+
+    var confirmLowercaseletters = window.confirm('Click OK to confirm including lowercase characters');
+
+    var confirmUppercaseletters = window.confirm('Click Ok to confirm including uppercase characters');
+
+    // -password length between 8-128
+    // -lowercase, uppercase, special characters
+    // 2 validate the input 
+    // 3 generate pword base off the criteria
+    // 4 display password on page
+    
+return "password will go here"
+}
+
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Write password to the #password input
 function writePassword() {
